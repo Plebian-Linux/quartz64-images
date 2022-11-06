120c5616e941b136f1528bba6856f5adfc932095
Nicolas F <ovdev@fratti.ch>
2022-11-06 12:41:12 +0100

    debos/scripts: remove more resources from image
    As per the systemd "Safely Building Images" guide, random-seed and
    credentials.secret should be removed as well.

--- debos-recipes/scripts/rootfs-cleanup.sh
+++ debos-recipes/scripts/rootfs-cleanup.sh
@@ -8,6 +8,9 @@ apt -y autoremove --purge
 apt clean
 
 # Remove SSH keys and machine ID so they get generated on first boot
+# also the random seed and credentials.secret
 rm -f /etc/ssh/ssh_host_* \
       /etc/machine-id \
-      /var/lib/dbus/machine-id
+      /var/lib/dbus/machine-id \
+      /var/lib/systemd/random-seed \
+      /var/lib/systemd/credential.secret
