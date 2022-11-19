1deb064741d00aa284feefe55b5deb664a362092
Nicolas F <ovdev@fratti.ch>
2022-11-19 21:15:39 +0100

    debos/scripts: Properly overwrite u-boot defaults file
    Appending to the file is not the way to go when there's a lot of
    stuff already in there, just overwrite it.

--- debos-recipes/scripts/set-u-boot-defaults.sh
+++ debos-recipes/scripts/set-u-boot-defaults.sh
@@ -12,7 +12,8 @@ elif [ "$BOARD" = "soquartz-model-a" ] ; then
     FDT_NAME="rk3566-soquartz-model-a.dtb"
 fi
 
-cat <<EOF >> /etc/default/u-boot
+cat <<EOF > /etc/default/u-boot
+## /etc/default/u-boot - configuration file for u-boot-update(8)
 
 #U_BOOT_UPDATE="true"
 
